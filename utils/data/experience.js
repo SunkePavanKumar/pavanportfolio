ab211d1a75a7932b327e14846187341e916a8376
pavankumarsunke <pavankumar.s@ngtalenttech.com>
2025-01-17 08:16:26 +0530

    COM : Update resume and experience"

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,24 +1,30 @@
 export const experiences = [
   {
-    id: 1,
-    title: 'Full Stack Developer',
-    company: "Self Employed (Freelancing)",
-    duration: "(Jan 2024- Present)"
+    id : 1,
+    title : "Full Stack Developer",
+    company : "NgTalentTech Group LLC",
+    duration : "(OCT-2024 - Present)"
   },
   {
     id: 2,
+    title: 'Full Stack Developer',
+    company: "Self Employed (Freelancing)",
+    duration: "(Jan 2024- Sept 2024)"
+  },
+  {
+    id: 3,
     title: "Backend Developer",
     company: "Epikindifi Software Solution",
     duration: "(Nov 2022 - Sept 2023)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Java Developer",
     company: "Wipro Talent Next",
     duration: "(May 2022 - Oct 2022)"
   },
   {
-    id : 4,
+    id : 5,
     title : "AWS Cloud Virtual Internship",
     duration : "(Oct 2021 - Dec 2021)",
     company : "AICTE EduSkills (AWS)"
